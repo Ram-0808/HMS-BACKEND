c88536d53638ad408f833af1ec5387340eb96885
Raghuram <raghuram@absolinsoft.com>
2026-06-04 16:54:09 +0530

    allowed hosts

--- hospital/settings.py
+++ hospital/settings.py
@@ -113,7 +113,9 @@ USE_TZ = True
 # Static & Media files
 # -------------------------------------------------------
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
